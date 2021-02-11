1bbcf5f47ae4fa3cbc4d9ad4dff9f5bfbc580b49
Thanh Trinh <thanchitrinh2000@gmail.com>
2021-02-11 19:04:55 +0700

    improve cat eat

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const Cat = require('./Cat');
-var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
+var Dog = require('./Dog');
 
-var dog = new Dog('Tom');
-var tom = new Cat();
-dog.eat(tom);
-console.log(dog);
+var cat = new Cat();
+var mouse = new Mouse('Mickey');
+var dog = new Dog();
+try {
+    cat.eat(dog);
+} catch {error} {
+  console.log('Error while cat eating a dog');
+}
+
+console.log(cat);
